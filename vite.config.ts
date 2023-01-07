b66b16ca26754b8e314508ebf1062f4f0e77bab3
Chris <boyd111c@gmail.com>
2023-01-07 17:18:52 -0600

    fix paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "./", //for proper relative links
     plugins: [tsconfigPaths(), react()],
 });
