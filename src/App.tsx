54c71b9a3ecbc9aa74d5c6c254494ddc0890cabf
Chris <boyd111c@gmail.com>
2023-01-07 23:41:43 -0600

    random tile gen with moving cam

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,90 @@
-import { OrbitControls, Sphere, Trail, useGLTF } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import { Mesh } from "three";
+import { useGLTF } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { PerspectiveCamera, Vector3 } from "three";
 import { RandomTile } from "./Tile";
+import { useEffect, useState } from "react";
 
-const Scene = () => {
-    const ref = useRef<Mesh>(null!);
-    useFrame(({ clock }) => {
-        const time = clock.getElapsedTime();
-        const delta = clock.getDelta();
-        ref.current.rotation.y += delta;
-        const pos = ref.current.position;
-        pos.y = 1;
-        pos.x = Math.cos(time);
-        pos.z = Math.sin(time * 2) / 2;
+function tilesIncludes(tiles: JSX.Element[], key: string): boolean {
+    let result = false;
+    tiles.findIndex((tile) => {
+        const k = tile.key;
+        if (typeof k === "string") {
+            if (k.includes(key)) {
+                result = true;
+                return true;
+            }
+        }
+    });
+    return result;
+}
+
+let index = 0;
+const Scene = (props: JSX.IntrinsicElements["group"]) => {
+    const [tiles, setTiles] = useState<JSX.Element[]>();
+    const new_tiles: JSX.Element[] = [];
+
+    const genTile = (x: number, z: number) => {
+        return <RandomTile position={[x, 0, z]} key={`tile:${index++},x:${x},z:${z}`} />;
+    };
+
+    useFrame((state, delta) => {
+        const cam_pos = state.camera.position;
+        state.camera.position.x += delta;
+        // state.camera.position.z -= delta;
+        new_tiles.length = 0;
+
+        if (!tiles) return;
+        for (const tile of tiles) {
+            const tile_pos = new Vector3(...tile.props.position);
+            if (tile_pos.x > cam_pos.x - 20) {
+                new_tiles.push(tile);
+            }
+        }
+        const size_z = 30;
+        const size_x = 20;
+        for (let x0 = size_x / 2; x0 <= size_x; x0++) {
+            const x = Math.ceil(x0 + cam_pos.x - size_x / 2);
+            if (tilesIncludes(tiles, `x:${x}`)) continue;
+            for (let z0 = 0; z0 < size_z; z0++) {
+                const z = Math.floor(z0 + cam_pos.z - size_z / 2);
+                if (!tilesIncludes(tiles, `x:${x},z:${z}`)) {
+                    new_tiles.push(genTile(x, z));
+                }
+            }
+        }
+        setTiles(new_tiles);
+        console.log(new_tiles.length);
+    });
+    useThree(({ camera }) => {
+        const target = new Vector3(0, -1, 0);
+        target.add(camera.position);
+        camera.lookAt(target);
     });
 
-    const tiles = [];
-    const size = 20;
-    for (let z = 0; z < size; z++) {
-        for (let x = 0; x < size; x++) {
-            tiles.push(<RandomTile position={[x - size / 2, 0, z - size / 2]} key={`x:${x},z${z}`} />);
+    useEffect(() => {
+        const size = 15;
+        const new_tiles = [];
+        for (let z = -size; z < size; z++) {
+            for (let x = -size; x < size; x++) {
+                new_tiles.push(genTile(x, z));
+            }
         }
-    }
+        setTiles(new_tiles);
+    }, []);
 
     return (
-        <>
+        <group {...props} dispose={null}>
             {tiles}
-            <Trail
-                width={1}
-                length={12}
-                color={"#F8D628"}
-                attenuation={(t: number) => {
-                    return t * t;
-                }}
-            >
-                <Sphere ref={ref} args={[0.1, 32, 16]} rotation={[0.5, 0, 0]}>
-                    <meshNormalMaterial />
-                </Sphere>
-            </Trail>
             <ambientLight color={[1, 1, 1]} intensity={0.2} />
             <directionalLight position={[1, 1, 1]} />
-        </>
+        </group>
     );
 };
 
 const App = () => {
     return (
-        <Canvas camera={{ fov: 70, position: [1, 3, 1] }}>
-            <OrbitControls />
+        <Canvas camera={{ fov: 70, position: [0, 25, 0] }}>
+            {/* <OrbitControls /> */}
             <Scene />
         </Canvas>
     );
